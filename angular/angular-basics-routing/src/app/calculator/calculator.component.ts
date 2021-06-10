aa665c443b9c5cb9a61ecf43a040d550748a2726
atanuc073 <atanuc073@gmail.com>
2021-06-10 11:33:14 +0530

    angular first project

---
+++ angular/angular-basics-routing/src/app/calculator/calculator.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css']
+})
+export class CalculatorComponent implements OnInit {
+  amt:number;
+  years:number;
+  rate:number;
+  res:string;
+  constructor() { }
+
+  ngOnInit() {
+  }
+  calcSimple():void{
+    this.res=(this.amt*this.years*this.rate/100.0).toFixed(2);
+
+  }
+  calcCompound():void{
+    this.res=(this.amt*Math.pow((1+this.rate)/100.0,this.years)-this.amt).toFixed(2);
+
+  }
+
+
+}
